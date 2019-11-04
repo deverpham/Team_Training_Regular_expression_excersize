a3252b7047008efeb24fa4ecbdbd047fae0b678c
= <letiendat1999py@gmail.com>
2019-11-04 19:07:07 +0700

    regex_dat

--- checksdt.js
+++ checksdt.js
@@ -1,3 +1,3 @@
-var pattenr = new RegExp(/^\+840[0-9]{9}$/);
-const string = '+840836486364';
-console.log(pattenr.test(string));
+var pattenr = new RegExp(/^\+84([1-9])([0-9]){9}$/);
+const string = '+842836486364';
+console.log(pattenr.test(string)); 
