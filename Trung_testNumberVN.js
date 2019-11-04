eb513957893c06f6066d2d13ba3a764973b0a805
voquoctrung1011 <voquoctrung1011@gmail.com>
2019-11-04 19:00:48 +0700

    Trung_regEx

--- Trung_testNumberVN.js
+++ Trung_testNumberVN.js
@@ -1,5 +1,5 @@
 function testNumberVN(){
-    var n=/^(\+84)[0-9]{9}$/;
-    console.log(n.test('+84935117425'));
+    var n=/^(\+84)[1-9][0-9]{8}$/;
+    console.log(n.test('+84035117425'));
 }
 testNumberVN();
